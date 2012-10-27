3e6b2e0bdbee94810214cee58a218f57fe10bd79
Ray Wenderlich <ray.wenderlich@gmail.com>
2012-10-27 12:29:41 -0400

    Initial Commit

---
+++ MonsterWars/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  MonsterWars
+//
+//  Created by Ray Wenderlich on 10/27/12.
+//  Copyright Razeware LLC 2012. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
